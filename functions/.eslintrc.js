3c01782ca27077f95e63478176422f6ecf121ffd
Souvik Biswas <sbis1999@gmail.com>
2022-03-16 17:58:18 +0530

    Update working functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -8,6 +8,14 @@ module.exports = {
     "eslint:recommended",
     "google",
   ],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
   rules: {
     quotes: ["error", "double"],
   },
--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,42 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const Mux = require("@mux/mux-node");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const {Video} = new Mux(
+    process.env.MUX_TOKEN_ID,
+    process.env.MUX_TOKEN_SECRET,
+);
+
+exports.createLiveStream = functions.https.onCall(
+    async (data, context) => {
+    // Checking that the user is authenticated.
+      if (!context.auth) {
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called " + "while authenticated.",
+        );
+      } else {
+        try {
+          const response = await Video.LiveStreams.create({
+            playback_policy: "public",
+            new_asset_settings: {playback_policy: "public"},
+          });
+
+          return response;
+        } catch (err) {
+          console.error(
+              `Unable to start the live stream ${context.auth.uid}. 
+          Error ${err}`,
+          );
+          throw new functions.https.HttpsError(
+              "aborted",
+              "Could not create live stream",
+          );
+        }
+      }
+    },
+);
