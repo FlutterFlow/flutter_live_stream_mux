49b0c453efdead2c64bcb3693cbc62b97e73b918
Souvik Biswas <sbis1999@gmail.com>
2022-03-17 18:20:50 +0530

    Add resources

--- lib/res/app_theme.dart
+++ lib/res/app_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final primary = ThemeData(
-    primarySwatch: Colors.indigo,
+    primarySwatch: Colors.pink,
     fontFamily: 'GoogleSans',
     backgroundColor: Colors.indigo,
     textTheme: const TextTheme(
@@ -18,4 +18,14 @@ class AppTheme {
       ),
     ),
   );
+}
+
+class CustomColors {
+  static const Color muxPink = Color(0xFFff1f78);
+  static const Color muxPinkLight = Color(0xFFff709a);
+  static const Color muxPinkVeryLight = Color(0xFFfef6f7);
+  static const Color muxOrange = Color(0xFFff4b03);
+  static const Color muxOrangeLight = Color(0xFFff7d6e);
+  static const Color muxGray = Color(0xFF383838);
+  static const Color muxGrayLight = Color(0xFFf9f9f9);
 }
---
+++ lib/res/strings.dart
@@ -0,0 +1,13 @@
+const String streamBaseURL = 'rtmps://global-live.mux.com:443/app/';
+
+// API for generating thumbnails of a video
+const String muxImageBaseUrl = 'https://image.mux.com';
+
+// API for streaming a video
+const muxStreamBaseUrl = 'https://stream.mux.com';
+
+// Thumbnail file type and size
+const imageTypeSize = 'thumbnail.jpg?time=5&width=200';
+
+// Received video file format
+const videoExtension = 'm3u8';
