349697c5766c9632aae2c8de0f9b95122d444dd9
Souvik Biswas <sbis1999@gmail.com>
2022-03-19 14:10:44 +0530

    Remove unused home page

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,7 @@ import 'package:flutter_mux_live/firebase_options.dart';
 import 'package:flutter_mux_live/res/app_theme.dart';
 import 'package:flutter_mux_live/screens/authentication/login_page.dart';
 import 'package:flutter_mux_live/screens/stream/dashboard_page.dart';
-import 'package:flutter_mux_live/utils/authentication_client.dart';
-import 'package:flutter_mux_live/utils/mux_client.dart';
 import 'package:video_stream/camera.dart';
-
-import 'screens/initial_page.dart';
 
 List<CameraDescription> cameras = [];
 void main() async {
@@ -37,58 +33,9 @@ class MyApp extends StatelessWidget {
       title: 'Live Stream',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.primary,
-      // home: const HomePage(),
       home: FirebaseAuth.instance.currentUser == null
           ? const LoginPage()
-          : DashboardPage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final MuxClient _muxClient;
-  late final AuthenticationClient _authClient;
-
-  @override
-  void initState() {
-    _muxClient = MuxClient();
-    _authClient = AuthenticationClient();
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(),
-          ElevatedButton(
-            onPressed: () async {
-              await _authClient.registerUsingEmailPassword(
-                name: 'Test User',
-                email: 'test@example.com',
-                password: '1234567',
-              );
-            },
-            child: const Text('Authenticate user'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              await _muxClient.createLiveStream();
-            },
-            child: const Text('Start stream'),
-          ),
-        ],
-      ),
+          : const DashboardPage(),
     );
   }
 }
